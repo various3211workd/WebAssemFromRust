a5e091af176d6d7a071b4b7a88d5783347d45303
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2018-11-05 07:58:51 +0900

    MAKE socket.io server & client template

---
+++ www/server.js
@@ -0,0 +1,43 @@
+var fs = require("fs");
+var http = require("http");
+var server = http.createServer();
+server.on("request", function(request, response){
+	//HTMLファイルをストリームで読み込む
+	var stream = fs.createReadStream("index.html");
+	response.writeHead(200, {"Content-Type":"text/html"});
+	stream.pipe(response);
+});
+server.listen(30000);
+
+// HTTPをWebSocketにUpgradeする
+var io = require("socket.io").listen(server);
+
+// ユーザ管理ハッシュ
+var userHash = {};
+
+// チャットアプリに接続した時に実行される
+io.on("connection", function(socket){
+
+	/** socketは接続しているブラウザで、以下のコードは接続直後にイベントを登録していく **/
+
+	// ブラウザ毎の接続開始周り。
+	socket.on("connected", function(name){
+		var msg = name + "が入室しました";
+		userHash[socket.id] = name;
+		io.emit("pushlish", {value: msg});
+	});
+
+	// メッセージ送信のイベント
+	socket.on("publish", function(data){
+		io.emit("publish", {value:data.value});
+	});
+
+	// ブラウザを閉じた等で退出イベント
+	socket.on("disconnect", function(){
+		if(userHash[socket.id]){
+			var msg = userHash[socket.id] + "が退出しました";
+			delete userHash[socket.id];
+			io.emit("publish", {value: msg});
+		}
+	});
+});
